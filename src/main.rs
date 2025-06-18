f476e252d0b212a73854569f0a23f64544032633
Lukas Elias Lund Majland <lukaslundmajland@gmail.com>
2025-06-18 19:44:30 +0200

    Now ratatui is a dependency and is working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use {
+    crossterm::event::{
+        self,
+        Event,
+    },
+    ratatui::{
+        prelude::*,
+        widgets::Block,
+    },
+};
+
+
+fn main() -> std::io::Result<()> {
+    let mut terminal = ratatui::init();
+    
+    loop {
+        terminal.draw(draw_frame)?;
+
+        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
+            break;
+        }
+    }
+
+    ratatui::restore();
+
+    Ok(())
+}
+
+fn draw_frame(frame: &mut Frame) {
+    frame.render_widget(Block::bordered().title("plotrat"), frame.area());
 }
